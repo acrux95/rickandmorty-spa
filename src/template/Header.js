938c9b1083c7081d777ffbfa625b693bbc7f7c50
acrux95 <67486473+ArmandoCloud@users.noreply.github.com>
2020-09-14 23:12:03 -0500

    Finishing my first SPA

---
+++ src/template/Header.js
@@ -0,0 +1,21 @@
+const Header = () => {
+    const view = `
+    <div class="Header-main">
+        <div class="Header-logo">
+            <h1>
+                <a href="/" class="">
+                    100tifi.co
+                </a>
+            </h1>
+        </div>
+        <div class="Header-nav">
+            <a href="#/about/" class="">
+                About
+            </a>
+        </div>
+    </div>
+    `;
+    return view;
+};
+
+export default Header;
